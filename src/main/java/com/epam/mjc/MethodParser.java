a9c2475d735b03ff941775dcb4ec3b24815022ea
Tigran124 <tigran.h.988@gmail.com>
2024-01-10 22:39:57 +0400

    changes completed

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,5 +1,8 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
@@ -20,6 +23,50 @@ public class MethodParser {
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String[] sig = signatureString.split(" ");
+        String name = "";
+        MethodSignature methodSignature;
+        if (sig[0].equals("public") || sig[0].equals("private")) {
+            for (int i = 0; i < sig[2].length(); i++) {
+                if ('(' == sig[2].charAt(i)) {
+                    name = sig[2].substring(0, i);
+                    break;
+                }
+            }
+            List<MethodSignature.Argument> argumentList = new ArrayList<>();
+            String[] arg = signatureString.split(name, 2);
+            if (arg[1].length() > 2) {
+                String[] args = arg[1].substring(1, arg[1].length() - 1).split(",");
+                for (String str : args) {
+                    String[] x = str.trim().split(" ");
+                    MethodSignature.Argument argument = new MethodSignature.Argument(x[0], x[1]);
+                    argumentList.add(argument);
+                }
+            }
+            methodSignature = new MethodSignature(name, argumentList);
+            methodSignature.setAccessModifier(sig[0]);
+            methodSignature.setReturnType(sig[1]);
+        }else {
+            for (int i = 0; i < sig[1].length(); i++) {
+                if ('(' == sig[1].charAt(i)) {
+                    name = sig[1].substring(0, i);
+                    break;
+                }
+            }
+            List<MethodSignature.Argument> argumentList = new ArrayList<>();
+            String[] arg = signatureString.split(name, 2);
+            if (arg[1].length() > 2) {
+                String[] args = arg[1].substring(1, arg[1].length() - 1).split(",");
+                for (String str : args) {
+                    String[] x = str.trim().split(" ");
+                    MethodSignature.Argument argument = new MethodSignature.Argument(x[0], x[1]);
+                    argumentList.add(argument);
+                }
+            }
+            methodSignature = new MethodSignature(name, argumentList);
+            methodSignature.setReturnType(sig[0]);
+        }
+        return methodSignature;
+//        throw new UnsupportedOperationException("You should implement this method.");
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,11 +9,30 @@ public class StringSplitter {
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> result = new ArrayList<>();
+        int startIndex = 0;
+        int endIndex;
+        String del = delimiters.toString();
+        for (int i = 0; i < source.length(); i++) {
+            char currentChar = source.charAt(i);
+            if (del.indexOf(currentChar) != -1) {
+                endIndex = i;
+                String temp = source.substring(startIndex, endIndex);
+                if (!temp.isEmpty()) {
+                    result.add(temp);
+                }
+                startIndex = i + 1;
+            }
+        }
+        String temp = source.substring(startIndex);
+        if (!temp.isEmpty()) {
+            result.add(temp);
+        }
+        return result;
     }
 }
